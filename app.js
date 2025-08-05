096fc7d55d0885d4b56f070db43c9efb3a75679f
F-krystian <krystianfpa@gmail.com>
2025-08-05 17:40:51 +0200

    math and calculator functions

--- app.js
+++ app.js
@@ -1,42 +1,137 @@
-let buttons = [...document.querySelectorAll('.calc_buttons-btn')];
-let displayPastNumber = document.querySelector('.calc_display_past-operant');
-let displayOperator = document.querySelector('.calc_display_operator');
-let displayCurrentNumber = document.querySelector('.calc_display_current-operant');
+let operationBtns = [...document.querySelectorAll('.calc_btn-operator')];
+let numberBtns  = [...document.querySelectorAll('.calc_btn-number')];
+let displayPreviousOperant = document.querySelector('.calc_display_past-operant');
+let displayOperation = document.querySelector('.calc_display_operator');
+let displayCurrentOperant = document.querySelector('.calc_display_current-operant');
+let sumBtn = document.querySelector('.calc-btn-sum');
+let deleteBtn  = document.querySelector('.btn_del');
+let clearBtn = document.querySelector('.btn_clear-entry');
+let clearAllBtn = document.querySelector('.btn_clear-all')
 
 
 let currentOperant = '';
-let currentOperator = '';
-let pastOperant = '';
+let previousOperant = '';
+let operation = '';
+
+let updateDisplay = function() {
+  displayCurrentOperant.innerText =  currentOperant;  
+  displayPreviousOperant.innerText = previousOperant;
+  displayOperation.innerText = operation; 
+}
 
 
-buttons.forEach((btn) => {
-  btn.addEventListener('click', () => {
-    if(btn.classList.contains('calc_btn-number')){
-      console.log('number');
-      let clickedNumber = btn.textContent;
-      console.log(clickedNumber);
-      if (clickedNumber === '.' && currentOperant === '') return;
-      if (clickedNumber === '.' && currentOperant.includes(".")) return;
+let appendNumber = function(number) {
+  if(!number) return;
+  currentOperant += number;
+  return currentOperant
+}
 
-      currentOperant += clickedNumber;
-      console.log(currentOperant)
-    }
+let chooseOperation = function(op) {
+  if(!op) return;
+  
+  if(!previousOperant && currentOperant) {
+    previousOperant = currentOperant;
+    currentOperant = '';
+  }
+  if(previousOperant && currentOperant) {
+    previousOperant = compute();
+  }
 
-    if(btn.classList.contains('calc_btn-operator')){
-      console.log('operator')
-    }
+  operation = op;
+  currentOperant = '';
+}
 
-    if(btn.classList.contains('calc_btn-fnc')){
-      console.log('functionality')
-    }
+let compute = function(){
+  let num1 = parseFloat(currentOperant);
+  let num2 = parseFloat(previousOperant);
+  console.log(num1);
+  console.log(num2);
 
-    if(btn.classList.contains('calc-btn-sum')){
-    console.log('calculate')
-    }
+  if (isNaN(num1) || isNaN(num2)) return;
+
+  let result = '';
+
+  switch (operation) {
+    case "+":
+      console.log("add");
+      result = num2 + num1;
+      break;
+    case "-":
+      console.log('minus');
+      result = num2 - num1;
+      break;
+    case "×":
+      console.log('multiply');
+      result = num2 * num1;
+      break;
+    case "÷":
+      console.log('divide');
+      if(num1 === 0){
+        result = 'Division by zero is undefined';
+        return result;
+      };
+      result = num2 / num1;
+      break;
+  }
+
+  return result;
+}
+
+
+let deleteNum = function() {
+  currentOperant = currentOperant.slice(0, -1);
+  updateDisplay();
+}
+
+let clearNum = function() {
+  currentOperant = '';
+  updateDisplay();
+}
+
+let clearAll = function (){
+  currentOperant = '';
+  previousOperant = '';
+  operation = '';
+  updateDisplay();
+}
+
+numberBtns.forEach((btn)=> { 
+   btn.addEventListener('click', (e)=> {
+    let clickedNumber =  e.target.innerText;
+    appendNumber(clickedNumber);
+    console.log(currentOperant);
+    updateDisplay();
+   })
+})
+
+operationBtns.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    let clickedOp = e.target.innerText;
+    chooseOperation(clickedOp);
+    updateDisplay();
   })
+})
 
-}) 
 
-//1 Get all Dom buttons
-//2 Get All 3 displays from DOM
-//3 Make variables for all data in use
+sumBtn.addEventListener('click', () => {
+
+  if(previousOperant && currentOperant && operation) {
+    previousOperant = compute();
+    operation = '';
+    currentOperant = '';
+    updateDisplay();
+  }
+})
+
+deleteBtn.addEventListener('click', () => {
+  deleteNum();
+})
+
+clearBtn.addEventListener('click', () => {
+  clearNum();
+})
+
+
+clearAllBtn.addEventListener('click', () => {
+  clearAll();
+})
